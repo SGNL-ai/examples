035be813695db61d98436886752d571850c97409
Aldo Pietropaolo <aldo@sgnl.ai>
2023-10-26 17:24:52 -0500

    updates to auth0 login action as per static code analysis

--- auth0/login-action.js
+++ auth0/login-action.js
@@ -1,47 +1,73 @@
 /**
 * Handler that will be called during the execution of a PostLogin flow.
 *
-* @param {Event} event - Details about the user and the context in which they are logging in.
-* @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
+* @param {Event} event - Details about the user and the context.
+* @param {PostLoginAPI} api - Auth0 Interface.
 */
 exports.onExecutePostLogin = async (event, api) => {
+  // Require the axios library
   const axios = require('axios');
-  
+
   // Create an instance of Axios HTTP client
 
-  const sgnl_api = axios.create({
-  baseURL: event.secrets.sgnl_url,
-  timeout: 1000,
-  headers: {'Authorization': event.secrets.Token}
+  const sgnlAPI = axios.create({
+    baseURL: event.secrets.sgnl_url,
+    timeout: 1000,
+    headers: {'Authorization': "Bearer " + event.secrets.Token}
   });
 
-  // Uncomment the line below to output requested resource to the Auth0 debug log
-  // console.log("URI: " + event.request.query.redirect_uri)
-
-  // Call SGNL access service API
-  await sgnl_api.post( event.secrets.sgnl_url,{
-	"principal": {
-		"id": event.user.email
-	},
-	"queries": [{
-		"assetId": event.request.query.redirect_uri,
+  if (isValidUrl(event.secrets.sgnl_url)) {
+    
+    // Call SGNL access service API
+    
+    await sgnlAPI.post( event.secrets.sgnl_url, {
+      "principal": {
+        "id": event.user.email
+      },
+      "queries": [{
+        "assetId": event.request.query.redirect_uri,
         "action": event.request.method
-	}]
-})
-  .then(function (response) {
-    console.log("SGNL policy decision: " + response.data.decisions[0].decision)
-   
-    //Deny access if SGNL policy results in a deny.
-    if (response.data.decisions[0].decision == "Deny") {
-    api.access.deny(`SGNL Authorization: Access to ${event.client.name} is not allowed.`);
-    api.access.deny
+      }]
+    })
+      .then(function(response) {
+          // Uncomment line below to print the SGNL authorization decision to the console log.
+          // console.log("SGNL policy decision: " + response.data.decisions[0].decision)
+
+          // Deny access if SGNL policy results in a deny.
+          if (response.data.decisions[0].decision == "Deny") {
+          api.access.deny(`SGNL Authorization: Access to ${event.client.name} is not allowed.`);
+          } else {
+          // Success. Allow the login flow to continue.
+          }
+          // uncomment for debug purposes.
+          // console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+          api.access.deny(`SGNL Authorization: Access to ${event.client.name} is not allowed.`);
+        });
   } else {
-    // Success
+      console.log(new Error('SGNL URL validation failed.'));
+      api.access.deny(`SGNL Authorization: Access to ${event.client.name} is not allowed.`);
+    }
+
+  /**
+ * Check for a valid SGNL URL.
+ * @param {url} string
+ * @return {boolean}
+ */
+  function isValidUrl(string) {
+    try {
+      const secretUrl = new URL(string);
+      
+      if (secretUrl.host === 'access.sgnlapis.cloud' && secretUrl.protocol === 'https:') {
+          return true;
+      } else {
+        return false
+      }
+    } catch (err) {
+      console.log(new Error('Error occured during URL validation.'));
+      return false;
+    }
   }
-   // uncomment for debug purposes
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
 };
