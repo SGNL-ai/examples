1582cded0a3370f74b00564d69e9ab3f7e3553b9
Atul Tulshibagwale <atul@sgnl.ai>
2025-02-25 09:00:41 -0600

    added code for the gateway profile to work

---
+++ authzen-interop/evaluation-1.1/README.md
@@ -0,0 +1,6 @@
+# SGNL AuthZEN v1.1 API Gateway Profile Demo Implementation
+This directory contains the source code of a Google Cloud Function that connects a subset of the proposed OpenID AuthZEN API v 1.1 to the SGNL Access API. It currently only supports the "/evaluations" endpoint of the AuthZEN API
+
+## Running the Code
+Simply copy the code from the `index.js` file into a Google Cloud Function that is configured to use Node.js. To run the function provide the bearer token required for the Access API as a bearer token in the authorization header of the HTTP request to this function.
+
---
+++ authzen-interop/evaluation-1.1/index.js
@@ -0,0 +1,281 @@
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('evaluation', (req, res) => {
+  const https = require("https");
+
+  // Check if request has a content-type header indicating JSON
+  if (!req.headers['content-type'] || !req.headers['content-type'].includes('application/json')) {
+    console.error('Error: Invalid request. Body needs to be of type application/json');
+    res.statusCode = 400;
+    res.send('Error: Invalid request. Body needs to be of type application/json');
+    return;
+  }
+
+  if (!req.headers['authorization'] || !req.headers['authorization'].startsWith('Bearer ')) { 
+    console.error('Error: Authorization header not found');
+    res.statusCode = 401;
+    res.send('Error: Authorization header not found');
+    return;
+  }
+  const bearerToken = req.headers['authorization'].split(' ')[1];
+
+  const options = {
+    "method": "POST",
+    "hostname": "access.sgnlapis.cloud",
+    "port": null,
+    "path": "/access/v2/evaluations",
+    "headers": {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      "X-Request-Id": "bfe9eb29-ab87-4ca3-be83-a1d5d8305716",
+      "Accept-Language": "en, en-US;q=0.8, es;q=0.7",
+      "Authorization": "Bearer " + bearerToken
+    }
+  };
+
+  const adminUsers = [
+    "CiRmZDA2MTRkMy1jMzlhLTQ3ODEtYjdiZC04Yjk2ZjVhNTEwMGQSBWxvY2Fs",
+    "CiRmZDE2MTRkMy1jMzlhLTQ3ODEtYjdiZC04Yjk2ZjVhNTEwMGQSBWxvY2Fs",
+    "CiRmZDI2MTRkMy1jMzlhLTQ3ODEtYjdiZC04Yjk2ZjVhNTEwMGQSBWxvY2Fs"
+  ]
+
+  
+  subject = getSubjectFromJsonBody(req.body);
+  if (subject === null) {
+    console.error('Error getting subject from request body');
+    res.send('Error getting subject from request body');
+    return;
+  }
+
+  action = getActionFromJsonBody(req.body);
+  if (action === null) {
+    console.error('Error getting action from request body');
+    res.send('Error getting action from request body');
+    return;
+  }
+
+  resourceType = getResourceTypeFromJsonBody(req.body);
+  if (resourceType === null) {
+    console.error('Error getting resource type from request body');
+    res.send('Error getting resource type from request body');
+    return;
+  }
+
+  if (resourceType == 'route') {
+    // Gateway profile
+    if (action == 'GET') {
+      console.log('Access granted');
+      res.statusCode = 200;
+      res.send("{\"decision\": true}");
+      return;
+    } else if (action == 'DELETE' || action == "POST" || action == "PUT") {
+      if (adminUsers.includes(subject)) {
+        res.statusCode = 200;
+        res.send("{\"decision\": true}");
+      } else {
+        res.statusCode = 200;
+        res.send("{\"decision\": false}");
+      }
+      return;
+    }
+  }
+
+  if (action == 'can_read_user' && resourceType != 'user') {
+    console.error('Error: action can_read_user is only allowed for resources of type user');
+    res.send('Error: action can_read_user is only allowed for resources of type user');
+    return;
+  } else if (action == 'can_read_user') {
+    userId = getResourceUserIDFromJsonBody(req.body);
+    if (userId == null) {
+      console.error('Error: user ID is required for this action');
+      res.send('Error: user ID is required for this action');
+      return;
+    }
+    assetId = userId;
+  } else {
+    // action relates to "todo", and not user
+    
+    if (resourceType != 'todo') {
+      console.error("Unknown resource type: " + resourceType);
+      res.send("Unknown resource type: " + resourceType);
+      return;
+    } else {
+      // resourceType is 'todo'
+      resourceOwnerID = getResourceOwnerIDFromJsonBody(req.body);
+      if ((action != 'can_create_todo' && action != 'can_read_todos') && resourceOwnerID == null) {
+        console.error('Error: resource owner ID is required for this action');
+        res.send('Error: resource owner ID is required for this action');
+        return;
+      } else {
+        assetId = resourceOwnerID;
+      }
+    }
+  }
+
+  const reqs = https.request(options, function (resp) {
+    const chunks = [];
+
+    resp.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    resp.on("end", function () {
+      const body = Buffer.concat(chunks);
+      const jsonBody = JSON.parse(body.toString());
+      if (typeof jsonBody === 'object' && 'decisions' in jsonBody) {
+        const decisions = jsonBody.decisions;
+        if (decisions.length > 0) {
+          const decision = decisions[0];
+          if ('decision' in decision) {
+            if (decision.decision == 'Allow') {
+              console.log('Access granted');
+              res.statusCode = 200;
+              res.send("{\"decision\": true}");
+              return;
+            } else {
+              console.log('Access denied');
+              res.statusCode = 200;
+              res.send("{\"decision\": false}");
+              return;
+            }
+          } else {
+            console.error('Error: "decision" field not found in decision');
+            res.statusCode = 500;
+            res.send('Error: "decision" field not found in decision');
+            return;
+          }
+        } else {
+          console.error('Error: No decisions found in response');
+          res.statusCode = 500;
+          res.send('Error: No decisions found in response');
+          return;
+        }
+      } else {
+        console.error('Error: Decisions not found in response');
+        console.log(body.toString());
+        res.statusCode = 500;
+        res.send('Error making request to SGNL Access API');
+        return;
+      }
+    });
+  });
+
+  reqs.write(JSON.stringify({
+    principal: {
+      id: subject
+    },
+    queries: [{action: action, assetId: assetId}]
+  }));
+  reqs.on('error', function (error) {
+    console.error('Error making request:', error.message);
+    res.statusCode = 500;
+    res.send('Error making request');
+  });
+  reqs.end();
+});
+
+function getSubjectFromJsonBody(body) {
+  try {
+    // Check if the parsed body is an object and has the "subject" field
+    if (typeof body === 'object' && 'subject' in body) {
+      const subject = body.subject;
+      if ('id' in subject) {
+        return subject.id;
+      } else {
+        console.error('Error getting subject from request body: "identity" field not found');
+        return null;
+      }
+    } else {
+      console.error('Error getting subject from request body: "subject" field not found');
+      return null;
+    }
+  } catch (error) {
+    // Handle potential parsing errors (invalid JSON)
+    console.error('Error parsing request body:', error.message);
+    return null;
+  }
+}
+
+function getActionFromJsonBody(body) {
+  try {
+    // Check if the parsed body is an object and has the "action" field
+    if (typeof body === 'object' && 'action' in body) {
+      const action = body.action;
+      if ('name' in action) {
+        return action.name;
+      } else {
+        console.error('Error getting action from request body: "name" field not found');
+        return null;
+      }
+    } else {
+      console.error('Error getting action from request body: "action" field not found');
+      return null;
+    }
+  } catch (error) {
+    // Handle potential parsing errors (invalid JSON)
+    console.error('Error parsing request body:', error.message);
+    return null;
+  }
+}
+
+function getResourceTypeFromJsonBody(body) {
+  try {
+    // Check if the parsed body is an object and has the "resource" field
+    if (typeof body === 'object' && 'resource' in body) {
+      const resource = body.resource;
+      if ('type' in resource) {
+        return resource.type;
+      } else {
+        console.error('Error getting resource from request body: "type" field not found');
+        return null;
+      }
+    } else {
+      console.error('Error getting resource from request body: "resource" field not found');
+      return null;
+    }
+  } catch (error) {
+    // Handle potential parsing errors (invalid JSON)
+    console.error('Error parsing request body:', error.message);
+    return null;
+  }
+}
+
+function getResourceOwnerIDFromJsonBody(body) {
+  try {
+    // Check if the parsed body is an object and has the "resource" field
+    if (typeof body === 'object' && 'resource' in body) {
+      const resource = body.resource;
+      if ('properties' in resource && 'ownerID' in resource.properties) {
+        return resource.properties.ownerID;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  } catch (error) {
+    // Handle potential parsing errors (invalid JSON)
+    console.error('Error parsing request body:', error.message);
+    return null;
+  }
+}
+
+function getResourceUserIDFromJsonBody(body) {
+  try {
+    // Check if the parsed body is an object and has the "resource" field
+    if (typeof body === 'object' && 'resource' in body) {
+      const resource = body.resource;
+      if ('id' in resource) {
+        return resource.id;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  } catch (error) {
+    // Handle potential parsing errors (invalid JSON)
+    console.error('Error parsing request body:', error.message);
+    return null;
+  }
+}
